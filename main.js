3e378e4294d4f604738578519402f5cc4bb5af4d
Nestor Peña <nesto1793@gmail.com>
2023-09-17 01:25:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Menu hamburguesa
+const nav = document.querySelector("#nav")
+const abrir = document.querySelector("#abrir")
+const cerrar = document.querySelector("#cerrar")
+
+
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("nav-visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("nav-visible");
+})
